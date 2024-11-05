197c82d9e9d2106fca006bb9d1d4efdd18e0226f
olvalaskevich <olvol269@gmail.com>
2024-11-05 13:03:35 +0300

    add handle error function

---
+++ src/common/utils/handle-error.ts
@@ -0,0 +1,7 @@
+// import {AppDispatch} from "../../app/store.ts";
+// import {isAxiosError} from "axios";
+//
+//
+// export const handleError=(e:any, dispatch:AppDispatch)=>{
+//     dispatch(setErrorAC(isAxiosError(e)?e.response?e.response.data.errorMessages[0].message:e.message:(e as Error).message))
+// }
